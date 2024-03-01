5d49689acde762cb5fe534d3470fa7ce1733ca33
Pattisier <Principursa@protonmail.com>
2024-03-01 16:00:46 -0700

    readded addy type

--- src/abis/Twine.ts
+++ src/abis/Twine.ts
@@ -1,6 +1,8 @@
+import { Address } from "viem";
+
 export const Contracts = {
-  creditVault: "0x4f13aC500c15e9E9EBF03428F51787C652EF2963",
-  metaAccount: "0x2C7E6Aa320E178aaFD9334ff1A0cd924234DcCE4",
-  ausdc: "0x16dA4541aD1807f4443d92D26044C1147406EB80",
-  usdc: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8",
+  creditVault: "0x4f13aC500c15e9E9EBF03428F51787C652EF2963" as Address,
+  metaAccount: "0x2C7E6Aa320E178aaFD9334ff1A0cd924234DcCE4" as Address,
+  ausdc: "0x16dA4541aD1807f4443d92D26044C1147406EB80" as Address,
+  usdc: "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8" as Address,
 };
